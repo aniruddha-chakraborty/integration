62e7058856cda3b5aad7e840ed132bc2e3f2df03
aniruddha-chakraborty <aniruddha.app@gmail.com>
2015-12-30 18:28:53 +0600

    updated cname

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,34 @@
 $(document).ready(function() {
+    var random = getRandomArbitrary(1,48);
 
-  
+
+    $('#dataimagequestion').on('click', function() { // code
+        
+
+    });
+
+}); // ready testing block ends
+
+
+function getRandomArbitrary(min, max) {
+
+    var randNumber = Math.random() * (max - min) + min;
+      if  (randNumber%2 == 1) {
+            if (randNumber == max) {
+
+                return randNumber - 1;
+
+            } else {
+
+                return randNumber + 1;
+
+            }
+      }
+}
+
+
+function getImage(number){
+
+  return 'data-image/'+number+'.jpg';
+
 }
