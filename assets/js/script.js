949c833af2b0a8ab9251a44c219213d138788b21
aniruddha-chakraborty <aniruddha.app@gmail.com>
2015-12-31 08:46:18 +0600

    updated system

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,17 +6,19 @@ var  question = ['1','3','5','7','9','11','13','15','17','19','21','23','25','27
 var  answer   = ['2','4','6','8','10','12','14','16','18','20','22','24','26','28','30','32','34','36','38'
                   ,'40' ,'42','44','46','48'];
 
+    $('#hideAnswer').hide();
+    $('#loading').hide();
     $('#refresh').on('click', function() {
 
           var random = getRandomArbitrary(0,24);
           var image  = getGenImage(random);
 
-          var loading = 'http://localhost/integration/data-image/0.gif';
-          $('#dataimagequestion').attr('src',loading);
-
+          $('#loading').show();
           $('#dataimagequestion').attr('correct',random);
           $('#dataimagequestion').attr('src',image);
           $('#answerquestion').html('?');
+          $('#loading').hide();
+
 
     });
 
@@ -25,25 +27,33 @@ var  answer   = ['2','4','6','8','10','12','14','16','18','20','22','24','26','2
         var correct = $('#dataimagequestion').attr('correct');
         var image   = getImage(correct);
         $('#answerquestion').html(image);
+        $('#showAnswer').hide();
+        $('#hideAnswer').show();
+
     });
 
     $('#hideAnswer').on('click',function(){
 
           $('#answerquestion').html('?');
+          $('#showAnswer').show();
+          $('#hideAnswer').hide();
     });
 
-    function getRandomArbitrary(min, max) {
+    function getRandomArbitrary(min, max)
+    {
         return Math.floor(Math.random() * (max - min) + min);
     }
 
 
-  function getGenImage(number){
+   function getGenImage(number)
+   {
 
     return 'data-image/'+question[number]+'.jpg';
 
   }
 
-  function getImage(number){
+  function getImage(number)
+  {
 
     return '<img src="data-image/'+answer[number]+'.jpg" height="300" width="300">';
 
